2b8d04a38c14398bea412ac61fd657f88e4eb8ed
MANOJ GUJJALA <balu88861@gmail.com>
2023-06-23 22:18:35 +0530

    factory

--- src/FactoryPattern/Client.java
+++ src/FactoryPattern/Client.java
@@ -1,11 +1,9 @@
 package src.FactoryPattern;
-
 import src.FactoryPattern.components.button.Button;
 import src.FactoryPattern.components.dropdown.Dropdown;
 import src.FactoryPattern.components.menu.Menu;
 
 public class Client {
-
     public static void main(String[] args) {
 
     Flutter flutter = new Flutter(SupportedPlatform.MAC);
